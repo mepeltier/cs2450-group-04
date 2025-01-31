2b19e89a7ac5011cf7bdec2d1f823f6064d398dd
Shawn Crook <shawncrook411@gmail.com>
2025-01-30 23:13:31 -0700

    Added basic starting tests

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -1,6 +1,7 @@
 import pytest
 import unittest
 from src.cpu import CPU
+from src.cpu import Halt
 
 class TestCPU(unittest.TestCase):
 
@@ -13,3 +14,53 @@ class TestCPU(unittest.TestCase):
     def test_CPU_init(self):
         assert isinstance(self.cpu, CPU)
 
+    def test_validate(self):
+        invalid_words = [-10_000, 10_000, -42.5, 1/2, 'Test', float(2)]
+        
+        for w in invalid_words:
+            with pytest.raises(ValueError):
+                CPU._validate(w)
+
+    def test_bad_operations(self):
+        bad_operations = [0, -10, -9999, 100, 999, 1200, 2200, 3400, 4400]
+
+        for o in bad_operations:
+            with pytest.raises(ValueError):
+                self.cpu.operation(o)
+
+    def test_halt(self):
+        with pytest.raises(Halt):
+            self.cpu.operation(4300)
+
+    def test_READ(self):
+        pass
+    
+    def test_WRITE(self):
+        pass
+
+    def test_LOAD(self):
+        pass
+
+    def test_STORE(self):
+        pass
+
+    def test_ARITHMATIC(self):
+        # ADD
+
+        # SUBTRACT
+
+        # MULTIPLY
+
+        # DIVIDE
+        pass
+
+    def test_BRANCH(self):
+        pass
+
+    def test_READ(self):
+        pass
+    
+
+
+      
+        
