b947b6213d791777ff9bfeb10059db4f86fca481
Micah Peltier <micah6_8@yahoo.com>
2025-02-04 15:52:25 -0700

    feat: update tests to account for changes
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -1,28 +1,31 @@
-import pytest # type: ignore
+import pytest  # type: ignore
 import unittest
 
 if __name__ != "__main__":
-    from src.cpu import CPU, MEMORY, IO
-    from src.cpu import Halt
+    from src.cpu import CPU, Halt
+    from src.memory import Memory
+    from src.boot import Bootstrapper
+
 
 class TestCPU(unittest.TestCase):
 
     def setUp(self):
-        self.cpu = CPU()            
-        MEMORY.load_program(CPU.read_file('tests/cpu_test.txt', fix_index=True))
+        boot = Bootstrapper()
+        self.cpu = boot.cpu
+        boot.load_program("tests/cpu_test.txt", fix_index=True)
 
     def tearDown(self):
-        pass        
+        pass
 
     def test_CPU_init(self):
         assert isinstance(self.cpu, CPU)
 
     def test_validate(self):
-        invalid_words = [-10_000, 10_000, -42.5, 1/2, 'Test', float(2)]
-        
+        invalid_words = [-10_000, 10_000, -42.5, 1 / 2, "Test", float(2)]
+
         for w in invalid_words:
             with pytest.raises(ValueError):
-                CPU._validate(w)
+                Memory.validate_word(w)
 
     def test_bad_operations(self):
         bad_operations = [0, -10, -9999, 100, 999, 1200, 2200, 3400, 4400]
@@ -36,22 +39,15 @@ class TestCPU(unittest.TestCase):
         Tests that the decypher word function is correclty splitting a 4-digit
         instruction into an operator and operand
         """
-        instructions = [
-            1000,
-            2000,
-            2300,
-            4599,
-            5301,
-            9999
-        ]
+        instructions = [1000, 2000, 2300, 4599, 5301, 9999]
 
         correct_operations_and_operands = [
             (10, 00),
             (20, 00),
             (23, 00),
-            (45, 99), 
-            (53,  1),
-            (99, 99)
+            (45, 99),
+            (53, 1),
+            (99, 99),
         ]
 
         for i, word in enumerate(instructions):
@@ -65,8 +61,10 @@ class TestCPU(unittest.TestCase):
                 CPU.decypher_instruction(i)
 
     def test_read_file(self):
-        self.cpu.read_from_memory(10) # Line 10 on .txt file - Memory file has index fixed in tests
-        assert self.cpu.register == 8 # Memory address contains the numer '+0008'
+        self.cpu.read_from_memory(
+            10
+        )  # Line 10 on .txt file - Memory file has index fixed in tests
+        assert self.cpu.register == 8  # Memory address contains the numer '+0008'
 
     def test_halt(self):
         with pytest.raises(Halt):
@@ -74,43 +72,43 @@ class TestCPU(unittest.TestCase):
 
     def test_READ(self):
         pass
-    
+
     def test_WRITE(self):
         pass
 
     def test_LOAD(self):
-        self.cpu.operation(2010) # Load from [10] 
+        self.cpu.operation(2010)  # Load from [10]
         assert self.cpu.accumulator == 8
 
-        self.cpu.operation(2011) # Load from [11] 
+        self.cpu.operation(2011)  # Load from [11]
         assert self.cpu.accumulator == 16
 
     def test_STORE(self):
-        self.cpu.operation(2010) # Load #0008 from [10] into accumulator
-        self.cpu.operation(2100) # Store #0008 into [00] in Memory
-        assert MEMORY.word_to_int(MEMORY.read(00)) == 8
+        self.cpu.operation(2010)  # Load #0008 from [10] into accumulator
+        self.cpu.operation(2100)  # Store #0008 into [00] in Memory
+        assert Memory.word_to_int(self.cpu.memory.read(00)) == 8
 
-        self.cpu.operation(2000) # Load #0008 from [00] into register
+        self.cpu.operation(2000)  # Load #0008 from [00] into register
         assert self.cpu.register == 8
 
     def test_ADD(self):
-        self.cpu.operation(2010) # Load #0008 from [10] into accumulator
-        self.cpu.operation(3011) # Add #0016 from [11] into accumulator
+        self.cpu.operation(2010)  # Load #0008 from [10] into accumulator
+        self.cpu.operation(3011)  # Add #0016 from [11] into accumulator
         assert self.cpu.accumulator == 8 + 16
 
     def test_SUBTRACT(self):
-        self.cpu.operation(2010) # Load #0008 from [10] into accumulator
-        self.cpu.operation(3112) # Subtract #0007 from [12] into accumulator
+        self.cpu.operation(2010)  # Load #0008 from [10] into accumulator
+        self.cpu.operation(3112)  # Subtract #0007 from [12] into accumulator
         assert self.cpu.accumulator == 8 - 7
 
     def test_MULTIPLY(self):
-        self.cpu.operation(2010) # Load #0008 from [10] into accumulator
-        self.cpu.operation(3312) # Multiply #0007 from [12] into accumulator
+        self.cpu.operation(2010)  # Load #0008 from [10] into accumulator
+        self.cpu.operation(3312)  # Multiply #0007 from [12] into accumulator
         assert self.cpu.accumulator == 8 * 7
 
     def test_DIVIDE(self):
-        self.cpu.operation(2011) # Load #0016 from [11] into accumulator
-        self.cpu.operation(3210) # Divide by #0008 from [10]
+        self.cpu.operation(2011)  # Load #0016 from [11] into accumulator
+        self.cpu.operation(3210)  # Divide by #0008 from [10]
         assert self.cpu.accumulator == 16 / 8
 
     def test_BRANCH(self):
@@ -118,34 +116,28 @@ class TestCPU(unittest.TestCase):
         assert self.cpu.pointer == 59
 
     def test_BRANCHNEG(self):
-        self.cpu.operation(2010) # Load #0008 from [10] into accumulator
-        self.cpu.operation(3110) # Subtract #0008 from accumulator (0)
-        self.cpu.operation(4259) # Branches to 59 if accumulator is ZERO
+        self.cpu.operation(2010)  # Load #0008 from [10] into accumulator
+        self.cpu.operation(3110)  # Subtract #0008 from accumulator (0)
+        self.cpu.operation(4259)  # Branches to 59 if accumulator is ZERO
         assert self.cpu.pointer == 59
-        
 
     def test_BRANCHZERO(self):
-        self.cpu.operation(2010) # Load #0008 from [10] into accumulator
-        self.cpu.operation(3111) # Subtract #0016 from accumulator (-8)
-        self.cpu.operation(4159) # Branches to 59 if accumulator is NEGATIVE
+        self.cpu.operation(2010)  # Load #0008 from [10] into accumulator
+        self.cpu.operation(3111)  # Subtract #0016 from accumulator (-8)
+        self.cpu.operation(4159)  # Branches to 59 if accumulator is NEGATIVE
         assert self.cpu.pointer == 59
-        
 
     def test_FULL_PROGRAM(self):
-        cpu = CPU()
-        cpu.run('tests/cpu_test.txt', fix_index=True)
-        final_data = cpu.read_file('tests/cpu_test_final.txt')
+        boot = Bootstrapper()
 
-        cpu.read_from_memory(85)
-        assert 1875 == cpu.register
-        
-        for i, word in enumerate(final_data):
-            cpu.read_from_memory(i)
-            assert MEMORY.word_to_int(word) == cpu.register
+        boot.load_program("tests/cpu_test.txt", fix_index=True)
+        boot.run()
+        with open("tests/cpu_test_final.txt") as file:
+            final_data = file.readlines()
 
+            boot.cpu.read_from_memory(85)
+            assert 1875 == boot.cpu.register
 
-
-
-
-      
-        
+            for i, word in enumerate(final_data):
+                boot.cpu.read_from_memory(i)
+                assert Memory.word_to_int(word.strip()) == boot.cpu.register
