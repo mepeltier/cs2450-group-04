b0990b937acf73391576a2dd055fed22ac63f8af
slaing26 <10841409@uvu.edu>
2025-02-08 15:08:14 -0700

    Updated the help message shown with the -h flag

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """UVSim main entry point"""
 
 import argparse
+import textwrap
 from typing import List
 try:
     from src.boot import Bootstrapper
@@ -12,14 +13,30 @@ io = boot.io
 mem = boot.memory
 cpu = boot.cpu
 
-parser = argparse.ArgumentParser(prog="UVSim", description="UVSim - Run a BasicML program")
-group = parser.add_mutually_exclusive_group()
+parser = argparse.ArgumentParser(prog="UVSim", 
+                                 formatter_class=argparse.RawDescriptionHelpFormatter, 
+                                 add_help=False,
+                                 usage="poetry run python src/main.py [file_path | -m] [-h] [-v] [--version]",
+                                 description=textwrap.dedent('''                        
+                                    This project is managed with Poetry. In order to run this project, first install Poetry, through 'pip install poetry' or one of the recommended methods described by its documentation. 
+                                    Once Poetry is installed, see the following commands:
 
+                                    $ poetry lock -- This step shouldn't be necessary, but it will make sure that the lockfile is up to date with the latest versions.
+
+                                    $ poetry install --all-extras -- This creates a .venv/ folder, in which poetry installs all the necessary libraries to run this project.
+                                                             
+                                    $ poetry run python src/main.py [file_path | -m] [-h] [-v] [--version]
+                                                             
+                                                             
+                                    UVSim - Run a BasicML program
+                                    --------------------------------              
+                                    '''))
+group = parser.add_mutually_exclusive_group()
 group.add_argument("file", type=str, action="store", nargs="?", metavar = "file_path", default = None, help="file path to the BasicML input file")
 group.add_argument("-m", "--manual", action="store_true", help="manually enter opcodes instead of reading from a file")
 parser.add_argument("-v", "--verbose", action="store_true", help="enable verbose mode for debugging")
 parser.add_argument("--version", action="version", version="%(prog)s 1.0")
-
+parser.add_argument("-h", "--help", action="help", help="show this help message and exit", )
 args = parser.parse_args()
 
 def main():
